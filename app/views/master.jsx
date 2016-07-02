36a1f8e965fe17d2faabc0db51fd5e18b6ab3e87
Dillon Jason <Dillon Jason>
2016-07-02 19:43:56 -0400

    Added master component and some style stuffs

--- app/views/master.jsx
+++ app/views/master.jsx
@@ -5,13 +5,23 @@ class Master extends React.Component {
     render() {
         return (
             <View>
-                <h1>Boiler Plate</h1>
-                <ul role="nav">
-                    <li><Link to="/Home">Home</Link></li>
-                    <li><Link to="/About">About</Link></li>
-                </ul>
+                <div className="pure-menu pure-menu-horizontal pure-menu-fixed">
+                    <Link to="/" className="pure-menu-heading pure-menu-link">BOILER PLATE</Link>
+                    <ul className="pure-menu-list">
+                        <li className="pure-menu-item">
+                            <Link to="/Home" className="pure-menu-link">Home</Link>
+                        </li>
+                        <li className="pure-menu-item">
+                            <Link to="/About" className="pure-menu-link">About</Link>
+                        </li>
+                    </ul>
+                </div>
 
-                {this.props.children}
+                <div className="content-wrapper">
+                    <div className="content">
+                        {this.props.children}
+                    </div>
+                </div>
             </View>
         );
     }
